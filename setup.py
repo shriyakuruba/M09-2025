91f8a6cc0d74cd16f84921aa8a695d650be89787
Shriya_Kuruba <ewu9af@virginia.edu>
2025-03-18 20:43:01 -0400

    Complete setup of demo package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/shriyakuruba/M09-2025',
+    author='Shriya Kuruba',
+    author_email='ewu9af@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
